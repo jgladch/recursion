fce175f746dece56d0d1bdf627861fedc68a239f
jgladch <jeffgladchun@gmail.com>
2014-08-14 13:28:13 -0700

    Recursive, but not working yet

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,26 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var object = obj;
+  var string = '{'
+
+  var inner = function(obj){
+  	if (Object.keys(obj).length > 0) {
+  		var key = Object.keys(obj)[0];
+  		var value = obj[Object.keys(obj)[0]];
+  		delete obj[Object.keys(obj)[0]];
+
+  		string = string+'"'+key+'": '+value+'';
+  		if (Object.keys(obj).length > 0){
+  			string = string+", "
+  		}
+  		inner(obj);
+  	} else{
+  		string = string+'}';
+  	};
+  };
+
+  inner(object);
+  return string;
+
 };
