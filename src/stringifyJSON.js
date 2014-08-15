00cacfd2bc7cb4601d3b74b0e9521843d34595e9
jgladch <jeffgladchun@gmail.com>
2014-08-15 13:12:16 -0700

    Passing stringify test! :)

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -18,8 +18,17 @@ var stringifyJSON = function(obj) {
 		for (var i = 0; i < keys.length; i++) {
 			var key = keys[i];
 			var value = stringifyJSON(object[keys[i]]);
-			objString = objString+'"'+key+'":'+value+'';
-			if (i + 1 < keys.length) {
+
+			console.log("key: "+key);
+			console.log("value: "+object[keys[i]]+" type: "+typeof object[keys[i]]);
+
+			if (typeof object[keys[i]] === 'function' || typeof object[keys[i]] === 'undefined') {
+
+			} else {
+				objString = objString+'"'+key+'":'+value+'';
+			};
+
+			if (i + 1 < keys.length && objString !== '{') {
 				objString = objString+",";
 			};
 		};
@@ -28,7 +37,8 @@ var stringifyJSON = function(obj) {
 	};
 
 	if (typeof obj === 'function' || typeof obj === 'undefined'){
-		return;
+		console.log("return null");
+		return '';
 	} else if (typeof obj === 'string') {
 		return '"'+obj+'"';
 	} else if (typeof obj === 'number' || typeof obj === 'boolean'){
@@ -44,159 +54,4 @@ var stringifyJSON = function(obj) {
 		return objToString(obj);
 	};
 
-	// CHAPTER 2 BEGINS HERE
-
-	// console.log("next object");
-	// console.log(obj);
-	// console.log(typeof obj);
-
-	// var objToString = function(obj){
-	// 	var objString = '{';
-
-	// 	if (Object.keys(obj).length > 0) {
-	//   	var key = Object.keys(obj)[0];
-	//   	var value = obj[Object.keys(obj)[0]];
-
-	//   	delete obj[Object.keys(obj)[0]];
-
-	//   	if (typeof value === 'string') {
-	//  			value = '"'+value+'"';
-	//  		}
-
-	//   	objString = objString+'"'+key+'": '+value+'';
-	//   	if (Object.keys(obj).length > 0){
-	//  			objString = objString+", "
-	//  		}
- //  		objToString(obj);
-	//   	} else {
-	//   		objString = objString+'}';
-	//   	}
-	//   	return objString;
-	// };
-
-	// var arrayToString = function(arr){
-	// 	console.log("in arrayToString")
-	// 	var arrString = '[';
-
-	// 	if (arr.length > 0) {
-	// 		for (var i = 0; i < arr.length; i++) {
-	// 			if (typeof arr[i] === 'number' || typeof arr[i] === 'boolean' ) {
-	// 				arrString = arrString+arr[i].toString()+',';
-	// 			} else if (typeof arr[i] === 'string'){
-	// 				arrString = arrString+'\"'+arr[i]+'\",';
-	// 			};
-
-	// 		};
-
-	// 		if (arrString.slice(-1) === ',') {
-	// 			arrString = arrString.slice(0,arrString.length - 1);
-	// 		};
-	// 		arrString = arrString + ']';
-	// 		console.log(arrString);
-	// 		return arrString;
-
-	// 	} else {
-	// 		arrString = arrString+']';
-	// 	};
-
-	// 	console.log(arrString);
-	// 	return arrString;
-	// };
-	
-	// if (typeof obj === 'number' || typeof obj === 'boolean') {
-	// 	return obj.toString();
-	// } else if (typeof obj === 'object'){
-	// 	if (obj === null) {
-	// 		console.log(obj);
-	// 		return "null";
-	// 	} else if (obj instanceof Array) {
-	// 		console.log("array to string");
-	// 		console.log(obj);
-	// 		return arrayToString(obj);
-	// 	};
-	// } else if (typeof obj === 'string'){
-	// 	return '"'+obj+'"';
-	// }
-
-	//CHAPTER 2 ENDS HERE
-
-	// Tests are currently failing with an empty array. typeof obj === 'object' and obj.toString() isn't working in that case
-
-
-
-	// var inner = function(obj){
-	//   if (Object.keys(obj).length > 0) {
-	//   		var key = Object.keys(obj)[0];
-	//   		var value = obj[Object.keys(obj)[0]];
-
-	//   		delete obj[Object.keys(obj)[0]];
-
-	//   		if (typeof value === 'string') {
-	//   			value = '"'+value+'"';
-	//   		}
-
-	//   		string = string+'"'+key+'": '+value+'';
-	//   		if (Object.keys(obj).length > 0){
-	//   			string = string+", "
-	//   		}
-	//   		inner(obj);
-	//   	} else{
-	//   		string = string+'}';
-	//   	}
-	// };
-
-	// if (typeof obj === 'object') {
-	// 	var object = obj;
- //  		var string = '{';
- //  		console.log(obj);
- //  		console.log(object);
- //  		console.log(window);
- //  		inner(object);
- //  		return string;
-	// } else if (typeof obj === 'number' || typeof obj === 'boolean'){
-	// 	var object = obj;
-	// 	var result = object.toString();
-	// 	return result;
-	// } else if (typeof obj === 'string'){
-	// 	return obj;
-	// };
-
-
-
- //  var object = obj;
- //  var string = '{';
-
-	//   var inner = function(obj){
-	//   	if (Object.keys(obj).length > 0) {
-	//   		var key = Object.keys(obj)[0];
-	//   		var value = obj[Object.keys(obj)[0]];
-
-	//   		delete obj[Object.keys(obj)[0]];
-
-	//   		if (typeof value === 'string') {
-	//   			value = '"'+value+'"';
-	//   		}
-
-	//   		string = string+'"'+key+'": '+value+'';
-	//   		if (Object.keys(obj).length > 0){
-	//   			string = string+", "
-	//   		}
-	//   		inner(obj);
-	//   	} else{
-	//   		string = string+'}';
-	//   	}
-	//   };
-
-	// if (typeof object === 'object') {
-	//   inner(object);
-	//   return string;
-	// } else if (typeof object === 'number' || typeof object === 'boolean'){
-	// 	console.log(object);
-	// 	console.log(string);
-	// 	var result = object.toString();
-	// 	return result;
-	//   return;
-	// } else {
-	//   return '\"'+object+'\"';
-	// }
 };
